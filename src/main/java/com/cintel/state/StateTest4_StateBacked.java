492e8343bf4a2474fbdbd3f4180f678c86efd3dc
e <1109566192@qq.com>
2022-03-15 22:49:43 +0800

    first commit

--- src/main/java/com/cintel/state/StateTest4_StateBacked.java
+++ src/main/java/com/cintel/state/StateTest4_StateBacked.java
@@ -1,9 +1,12 @@
 package com.cintel.state;
 
 import com.cintel.beans.SensorReading;
+import org.apache.flink.api.common.restartstrategy.RestartStrategies;
+import org.apache.flink.api.common.time.Time;
 import org.apache.flink.contrib.streaming.state.RocksDBStateBackend;
 import org.apache.flink.runtime.state.filesystem.FsStateBackend;
 import org.apache.flink.runtime.state.memory.MemoryStateBackend;
+import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
@@ -19,6 +22,42 @@ public class StateTest4_StateBacked {
         env.setStateBackend(new MemoryStateBackend());
         env.setStateBackend(new FsStateBackend("hdfs:///flink/checkpoint"));
         env.setStateBackend(new RocksDBStateBackend(""));
+
+        // 配置检查点  默认500ms
+        env.enableCheckpointing(300);
+
+        // 高级选项
+
+        // checkpoint mode (精准一次，至少一次)
+        env.getCheckpointConfig().setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
+        // 超时时间
+        env.getCheckpointConfig().setCheckpointTimeout(60000L);
+        // 同时执行的个数  前一个checkpoint可能还没完， 后一个可能已经触发了   默认是1
+        env.getCheckpointConfig().setMaxConcurrentCheckpoints(2);
+
+        // 一个checkpoint完成以后， 最小间隔这个时间以后才会执行下一次checkpoint ,该配置可以覆盖配置的检查点间隔时间
+        // 每300ms checkpoint一次，如果这次用时250ms，下次会在50ms以后执行，但是有了这个配置，必须在100ms以后才能执行
+        // 如果用时执行checkpoint执行用时150ms,剩余的时间超过了100ms,那么正常执行，下次在150ms以后
+        env.getCheckpointConfig().setMinPauseBetweenCheckpoints(100L);
+
+        // 如果设置为true,恢复的时候会从最近的检查点恢复，而不是保存点
+        // 默认是false 恢复的时候不管是保存点还是检查点，从最近的一个去恢复
+        env.getCheckpointConfig().setPreferCheckpointForRecovery(true);
+
+        // 默认0 可以容忍checkpoint失败几次以后，认为任务失败
+        env.getCheckpointConfig().setTolerableCheckpointFailureNumber(3);
+
+        // 重启策略
+        // 回滚重启 基于平台的   例如yarn的重启机制
+        env.setRestartStrategy(RestartStrategies.fallBackRestart());
+        // 不重启
+        env.setRestartStrategy(RestartStrategies.noRestart());
+        // 固定延迟重启 每个10s重启一次，重启3次总共
+        env.setRestartStrategy(RestartStrategies.fixedDelayRestart(3,10000L));
+        // 在10分钟之内,每次重启间隔1分钟，只尝试3次                      重试次数       时间段内         重试间隔
+        env.setRestartStrategy(RestartStrategies.failureRateRestart(3, Time.minutes(10),Time.minutes(1)));
+
+
 
 
         DataStream<String> inputStream = env.socketTextStream("192.168.128.131", 6666);
---
+++ src/main/java/com/cintel/tableapi/Example.java
@@ -0,0 +1,53 @@
+package com.cintel.tableapi;
+
+import com.cintel.beans.SensorReading;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.java.StreamTableEnvironment;
+import org.apache.flink.types.Row;
+
+/**
+ * table api
+ */
+public class Example {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        // 1. 读取数据
+        DataStreamSource<String> inputStream = env.readTextFile("F:\\work\\workspaces\\idea\\myflink\\src\\main\\resources\\sensors.txt");
+
+        // 2. 转换成POJO
+        DataStream<SensorReading> dataStream = inputStream.map(line -> {
+            String[] fields = line.split(",");
+            return new SensorReading(fields[0], Long.valueOf(fields[1]), Double.valueOf(fields[2]));
+        });
+
+
+        // 3. 创建表环境
+        StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env);
+
+        // 4. 基于流创建一张表
+        Table dataTable = tableEnv.fromDataStream(dataStream);
+
+        // 5. 调用table api 进行操作
+        Table resultTable = dataTable.select("id,temperature")
+                .where("id = 'sonser_1'");
+
+        // 6. 执行sql  先注册
+        tableEnv.createTemporaryView("sensor",dataTable);
+        String sql = "select id,temperature from sensor where id = 'sonser_1'";
+        Table resultSqlTable = tableEnv.sqlQuery(sql);
+
+        //输出结果
+        tableEnv.toAppendStream(resultTable, Row.class).print("resultTable");
+        tableEnv.toAppendStream(resultSqlTable, Row.class).print("resultSqlTable");
+
+
+
+        env.execute();
+
+    }
+}
